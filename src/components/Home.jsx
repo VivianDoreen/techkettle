3e3f37f06df8967bb01ce7661dff5eb34c53fe5a
viviandoreen <nabulovivian2014@gmail.com>
2021-01-07 15:09:11 +0300

    put testimonials just above the the get in touch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,9 +28,9 @@ export class Home extends Component {
         <Header data={this.state.landingPageData.Header} />
         <About data={this.state.landingPageData.About} />
         <Services data={this.state.landingPageData.Services} />  
-        <Testimonials data={this.state.landingPageData.Testimonials} />
         {/* <Gallery /> */}
         <WhyChooseUs data={this.state.landingPageData.WhyChooseUs} />
+        <Testimonials data={this.state.landingPageData.Testimonials} />
         <Contact data={this.state.landingPageData.Contact} />
       </div>
     )
