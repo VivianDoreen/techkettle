c413f9f13181e560acf14572dd13a24b9cb5767b
viviandoreen <nabulovivian2014@gmail.com>
2021-02-05 00:31:41 +0300

    modify the testimonies section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,23 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class about extends Component {
   render() {
     return (
-        <div id="about">
+      <div id="about">
         <div className="container">
           <div className="row">
-            <div className="col-xs-12 col-md-6"> <img src="img/about.jpg" className="img-responsive" alt=""/> </div>
+            <div className="col-xs-12 col-md-6">
+              {' '}
+              <img
+                src="img/about-us.png"
+                className="img-responsive"
+                alt=""
+              />{' '}
+            </div>
             <div className="col-xs-12 col-md-6">
               <div className="about-text">
                 <h2>About Us</h2>
-                <p style={{textAlign:'justify'}}>{this.props.data ? this.props.data.paragraph : 'loading...'}</p>
+                <p style={{ textAlign: 'justify' }}>
+                  {this.props.data ? this.props.data.paragraph : 'loading...'}
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default about
+export default about;
